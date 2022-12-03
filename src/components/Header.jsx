8b03583522f46b86636d137856f3e4a333a6565a
francolind <90268531+francolind@users.noreply.github.com>
2022-12-03 08:49:03 -0300

    añadiendo menu hamburger

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,20 @@ import '../style/Header.css';
 import search from '../assets/search.png';
 import menu from '../assets/menuW.png';
 import Logo from '../assets/Logo.png'
+import close from '../assets/closeW.png'
+import { useState } from 'react';
 
 export const Header = () => {
+    const [actMenu,setActMenu] = useState(true);
+    const toggleMenu = ()=>{
+        setActMenu(!menu)
+    }
     return(
             <div className="header-conteiner">
                 <div className="header-logo">
                     <img src={Logo} alt="Logo" />
                 </div>
-                <div className="header-menu">
+                <div className={`header-menu ${actMenu ? 'isActive' : ''} `}>
                     <ul>
                         <li>home</li>
                         <li>about</li>
@@ -17,8 +23,10 @@ export const Header = () => {
                         <li>contact</li>
                     </ul>
                 </div>
-                <div className='header-menu-hamburger'>
-                    <img src={menu} alt="" />
+                <div className='button-menu' onClick={toggleMenu}>{
+                    actMenu ? (<img className='button-menu-ham' src={close} alt="" />) :
+                    (<img className='button-menu-close' src={menu} alt="" />)
+                }
                 </div>
                 <div className='header-search'>
                     <input type="text" placeholder="search..." />
@@ -27,3 +35,4 @@ export const Header = () => {
             </div> 
     )
 };
+// {`header-menu ${true ? 'isActive' : ''} `}
